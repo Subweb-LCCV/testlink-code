a5e34de22b221beef98643622e7ec13f6e4dab11
Marco Antonio <marco.silva@lccv.ufal.br>
2025-10-12 01:46:10 -0300

    Update email_api.php

--- lib/functions/email_api.php
+++ lib/functions/email_api.php
@@ -116,7 +116,7 @@ function email_send( $p_from, $p_recipient, $p_subject, $p_message, $p_cc='',
       if ( !is_blank( config_get( 'smtp_username' ) ) ) {
         # Use SMTP Authentication
         $mail->SMTPAuth = true;
-        $mail->AuthType = 'PLAIN';
+        $mail->AuthType = 'LOGIN';
         $mail->Username = config_get( 'smtp_username' );
         $mail->Password = config_get( 'smtp_password' );
       }
